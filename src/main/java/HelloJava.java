0cd890b70df5dda3bcdcd85514c8cdc6701a3741
PAS L'IUT <noreply@univ-tlse2.fr>
2022-09-08 09:22:59 +0200

    super modifs

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -7,5 +7,6 @@ class HelloJava {
 	public afficherCancan(){
 		System.out.println("c tp ai suuuuuuuuperre!");
 		System.out.println("Je dirai meme plus, super.");
+		System.out.println("et pourquoi pas sus père");
 	}
 }
